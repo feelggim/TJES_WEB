15d481fb08a38cac2cdda3a40487ffd91e312d80
feelggim <feelggim@gmail.com>
2024-03-21 11:38:50 +0900

    이미지슬라이드배운거

---
+++ Day09/01_이미지슬라이드/swiper/js/script.js
@@ -0,0 +1,31 @@
+$(function () {
+
+
+    // 스와이퍼 시작!
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'vertical',
+        loop: true,
+
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
+
+
+})
